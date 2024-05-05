980651abff5c3c735ab00b68e97ad97c3aa78b7a
Matiki <maciej.tomasz.krzeminski@gmail.com>
2024-05-05 12:45:56 +0200

    Add pub struct User and auto counter of suty slots and users

--- d/src/d_backend/src/lib.rs
+++ d/src/d_backend/src/lib.rs
@@ -52,6 +52,42 @@ impl Storable for DutySlot {
     };
 }
 
+// A storable type of User
+#[derive(CandidType, Deserialize, Clone)]
+pub struct User {
+    pub username: String,
+    pub password: String,
+    pub role: UserRole,
+    pub specialty: Option<usize>,
+    pub localization: Option<String>,
+    pub profile_visible: bool,
+    pub email: Option<String>,
+    pub phone_number: Option<String>,
+}
+
+impl Storable for User {
+    fn to_bytes(&self) -> Cow<[u8]> {
+        Cow::Owned(Encode!(self).unwrap())
+    }
+
+    fn from_bytes(bytes: Cow<[u8]>) -> Self {
+        Decode!(bytes.as_ref(), Self).unwrap()
+    }
+
+    const BOUND: Bound = Bound::Bounded {
+        max_size: MAX_VALUE_SIZE,
+        is_fixed_size: false,
+    };
+}       
+
+
+
+#[derive(CandidType, Deserialize, Clone)]
+pub enum UserRole {
+    Doctor,
+    Hospital,
+}
+
 thread_local! {
     static MEMORY_MANAGER: RefCell<MemoryManager<DefaultMemoryImpl>> =
         RefCell::new(MemoryManager::init(DefaultMemoryImpl::default()));
@@ -61,20 +97,41 @@ thread_local! {
             MEMORY_MANAGER.with(|m| m.borrow().get(MemoryId::new(0))),
         )
     );
+    static NEXT_DUTYSLOTS_KEY: RefCell<u32> = RefCell::new(1);
 
     static SPECIALTIES: RefCell<Vec<String>> = RefCell::new(SPECIALTIES_STRINGS.iter().map(|&s| s.to_string()).collect());
+
+    // User map
+    static USER_MAP: RefCell<StableBTreeMap<u32, User, Memory>> = RefCell::new(
+        StableBTreeMap::init(
+            MEMORY_MANAGER.with(|m| m.borrow().get(MemoryId::new(1))),
+        )
+    );
+    // Next user key
+    static NEXT_USER_KEY: RefCell<u32> = RefCell::new(1);
+
+
+
+
 }
 
 
 /// Retrieves all DutySlots as a Vec.
 #[ic_cdk_macros::query]
-fn get_all() -> Vec<DutySlot> {
+fn get_all_duty_slots() -> Vec<DutySlot> {
     MAP.with(|p| p.borrow().iter().map(|(_, v)| v.clone()).collect())
 }
 
 #[ic_cdk_macros::update]
-fn insert(key: u32, value: DutySlot) -> Option<DutySlot> {
-    MAP.with(|p| p.borrow_mut().insert(key, value))
+fn insert_duty_slot(value: DutySlot) -> u32 {
+    let key = NEXT_DUTYSLOTS_KEY.with(|k| {
+        let key = *k.borrow();
+        *k.borrow_mut() += 1;
+        key
+    });
+
+    MAP.with(|p| p.borrow_mut().insert(key, value));
+    key
 }
 
 // Retirieve all specialties
@@ -82,6 +139,26 @@ fn insert(key: u32, value: DutySlot) -> Option<DutySlot> {
 fn get_specialties() -> Vec<String> {
     SPECIALTIES.with(|p| p.borrow().clone())
 }
+
+// Insert a new user
+#[ic_cdk_macros::update]
+fn insert_user(value: User) -> u32 {
+    let key = NEXT_USER_KEY.with(|k| {
+        let key = *k.borrow();
+        *k.borrow_mut() += 1;
+        key
+    });
+
+    USER_MAP.with(|p| p.borrow_mut().insert(key, value));
+    key
+}
+
+// Get all users
+#[ic_cdk_macros::query]
+fn get_all_users() -> Vec<User> {
+    USER_MAP.with(|p| p.borrow().iter().map(|(_, v)| v.clone()).collect())
+}
+
 
 
 ic_cdk::export_candid!();
