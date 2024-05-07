688dadf6d22204aef1fc85006c77ae104b27d2d9
Matiki <maciej.tomasz.krzeminski@gmail.com>
2024-05-07 17:47:50 +0200

    Format respones as compacj json

--- test.sh
+++ test.sh
@@ -16,13 +16,13 @@ webserver_port=$(jq -r '.webserver_port' $file_name)
 # JSON array of records
 json_array='[
     {
-        "data": "{ \"hello\": \"world\" }",
-        "expected_response": "{\"dutySlots\":[],\"message\":\"Hello World from GET /duty/slots/json\",\"statusCode\":200}",
+        "data": { "hello": "world" },
+        "expected_response": {"dutySlots":[],"message":"Hello World from GET /duty/slots/json","statusCode":200},
         "endpoint": "/duty/slots/json",
         "command": "GET"
     },
     {
-        "data": "{ \"username\": \"D1\", \"password\": \"a\", \"role\":  \"Doctor\", \"specialty\": 12, \"localization\": \"example_localization\" }",
+        "data": { "username": "D1", "password": "a", "role":  "Doctor", "specialty": 12, "localization": "example_localization" },
         "expected_response": "{\"key\":1,\"message\":\"User registered\",\"statusCode\":200}",
         "endpoint": "/auth/register",
         "command": "POST"
@@ -40,6 +40,7 @@ for (( i=0; i<$length; i++ )); do
     record=$(jq -c ".[$i]" records.json)
     data=$(echo $record | jq -r '.data')
     expected_response=$(echo $record | jq -r '.expected_response')
+    expected_response=$(echo $expected_response | jq -c .)
     endpoint=$(echo $record | jq -r '.endpoint')
     command=$(echo $record | jq -r '.command')
 
@@ -47,6 +48,7 @@ for (( i=0; i<$length; i++ )); do
     curl_command="curl -X $command -H \"Content-Type: application/json\" -d '$data' http://$canister_id.localhost:$webserver_port$endpoint"
     echo $curl_command
     response=$(eval $curl_command)
+    response=$(echo $response | jq -c .)
 
     echo "Response from server:"
     echo $response 
