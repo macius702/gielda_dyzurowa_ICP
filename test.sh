f97efdd71806327b362ea3c2b8e88706dd34e17b
Matiki <maciej.tomasz.krzeminski@gmail.com>
2024-05-07 17:32:37 +0200

    Move all tests data in one json array

--- test.sh
+++ test.sh
@@ -13,45 +13,54 @@ file_name=$1
 canister_id=$(jq -r '.canister_id' $file_name)
 webserver_port=$(jq -r '.webserver_port' $file_name)
 
-declare -a data
-declare -a expected_responses
-declare -a endpoints
-declare -a commands
+# JSON array of records
+json_array='[
+    {
+        "data": "{ \"hello\": \"world\" }",
+        "expected_response": "{\"dutySlots\":[],\"message\":\"Hello World from GET /duty/slots/json\",\"statusCode\":200}",
+        "endpoint": "/duty/slots/json",
+        "command": "GET"
+    },
+    {
+        "data": "{ \"username\": \"D1\", \"password\": \"a\", \"role\":  \"Doctor\", \"specialty\": 12, \"localization\": \"example_localization\" }",
+        "expected_response": "{\"key\":1,\"message\":\"User registered\",\"statusCode\":200}",
+        "endpoint": "/auth/register",
+        "command": "POST"
+    }
+]'
 
-# Add your data, expected responses, endpoints, and commands here
-data[0]='{ "hello": "world" }'
-expected_responses[0]='{"dutySlots":[],"message":"Hello World from GET /duty/slots/json","statusCode":200}'
-endpoints[0]="/duty/slots/json"
-commands[0]="GET"
+# Save the JSON array to a file
+echo $json_array > records.json
 
-data[1]='{
-  "username": "D1",
-  "password": "a",
-  "role":  "Doctor",
-  "specialty": 12,
-  "localization": "example_localization"
-}'
-expected_responses[1]='{"key":1,"message":"User registered","statusCode":200}'
-endpoints[1]="/auth/register"
-commands[1]="POST"
+# Get the length of the array
+length=$(jq '. | length' records.json)
 
-# Loop over the arrays
-for i in "${!data[@]}"; do
+# Loop over the array
+for (( i=0; i<$length; i++ )); do
+    record=$(jq -c ".[$i]" records.json)
+    data=$(echo $record | jq -r '.data')
+    expected_response=$(echo $record | jq -r '.expected_response')
+    endpoint=$(echo $record | jq -r '.endpoint')
+    command=$(echo $record | jq -r '.command')
+
     echo "Issuing curl command for testing..."
-    curl_command="curl -X ${commands[$i]} -H \"Content-Type: application/json\" -d '${data[$i]}' http://$canister_id.localhost:$webserver_port${endpoints[$i]}"
+    curl_command="curl -X $command -H \"Content-Type: application/json\" -d '$data' http://$canister_id.localhost:$webserver_port$endpoint"
     echo $curl_command
     response=$(eval $curl_command)
 
     echo "Response from server:"
     echo $response 
 
-    if [ "$response" == "${expected_responses[$i]}" ]; then
+    if [ "$response" == "$expected_response" ]; then
         echo "Test passed"
     else
         echo "Test failed"
         echo "Expected response:"
-        echo "${expected_responses[$i]}"
+        echo "$expected_response"
         echo "Actual response:"
         echo $response
     fi
 done
+
+# Remove the temporary file
+rm records.json
