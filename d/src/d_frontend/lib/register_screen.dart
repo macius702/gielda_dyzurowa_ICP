6e959b0f8e204d1da68877d9a9b4d97af20853f1
Matiki <maciej.tomasz.krzeminski@gmail.com>
2024-06-09 07:01:42 +0200

    Refactor RegisterScreen

--- d/src/d_frontend/lib/register_screen.dart
+++ d/src/d_frontend/lib/register_screen.dart
@@ -19,7 +19,29 @@ class RegisterScreen extends StatefulWidget {
 
 class _RegisterScreenState extends State<RegisterScreen> {
 
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Register'),
+      ),
+      drawer: CommonDrawer(),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: RegisterForm(),
+      ),
+    );
+  }
+}
+
+class RegisterForm extends StatelessWidget {
+
   final RegisterStore _registerStore = RegisterStore();
+  // ignore: use_super_parameters
+  RegisterForm({
+    Key? key,
+  }) : super(key: key);
 
   final TextEditingController _usernameController = TextEditingController();
   final TextEditingController _passwordController = TextEditingController();
@@ -30,137 +52,128 @@ class _RegisterScreenState extends State<RegisterScreen> {
   Widget build(BuildContext context) {
     final counterStore = Provider.of<CounterStore>(context);
     return Observer( 
-      builder: (_) => Scaffold(
-      appBar: AppBar(
-        title: const Text('Register'),
-      ),
-      drawer: CommonDrawer(),
-      body: Padding(
-        padding: const EdgeInsets.all(16.0),
-        child: Column(
-          children: <Widget>[
-            TextField(
-              key: const Key('usernameField'),
-              onChanged : _registerStore.setUsername,
-              controller: _usernameController,
-              decoration: const InputDecoration(
-                labelText: 'Username',
-              ),
-            ),
-            TextField(
-              key: const Key('passwordField'),
-              onChanged: _registerStore.setPassword,
-              controller: _passwordController,
-              decoration: const InputDecoration(
-                labelText: 'Password',
-              ),
-              obscureText: true,
-            ),
+      builder: (_) => Column(
+      children: <Widget>[
+        TextField(
+          key: const Key('usernameField'),
+          onChanged : _registerStore.setUsername,
+          controller: _usernameController,
+          decoration: const InputDecoration(
+            labelText: 'Username',
+          ),
+        ),
+        TextField(
+          key: const Key('passwordField'),
+          onChanged: _registerStore.setPassword,
+          controller: _passwordController,
+          decoration: const InputDecoration(
+            labelText: 'Password',
+          ),
+          obscureText: true,
+        ),
 
-            DropdownButton<String>(
-              key: const Key('roleDropdown'),
-              value: _registerStore.role,
-              hint: const Text('Select Role'),
-              items: _roles.map((String value) {
-                return DropdownMenuItem<String>(
-                  value: value,
-                  child: Text(value),
+        DropdownButton<String>(
+          key: const Key('roleDropdown'),
+          value: _registerStore.role,
+          hint: const Text('Select Role'),
+          items: _roles.map((String value) {
+            return DropdownMenuItem<String>(
+              value: value,
+              child: Text(value),
+            );
+          }).toList(),
+          onChanged: _registerStore.setRole
+          ,
+        ),
+        if (_registerStore.role == 'doctor')
+          SpecialtyDropdownMenu(specialties: counterStore.specialties,  onSelected: _registerStore.setSpecialty),
+
+        if (_registerStore.role == 'doctor')
+          TextField(
+            onChanged : _registerStore.setLocalization,
+            controller: _localizationController,
+            decoration: const InputDecoration(
+              labelText: 'Localization',
+            ),
+          ),
+          ElevatedButton(
+            key: const Key('registerButton'),
+            onPressed: () async {
+              // Store the BuildContext in a local variable
+              final localContext = context;
+              if (_registerStore.username.isEmpty || _registerStore.password.isEmpty || _registerStore.role == null || _registerStore.role!.isEmpty) {
+                ScaffoldMessenger.of(context).showSnackBar(
+                  const SnackBar(content: Text("Username, password, and role are mandatory")),
                 );
-              }).toList(),
-              onChanged: _registerStore.setRole
-              ,
-            ),
-            if (_registerStore.role == 'doctor')
-              SpecialtyDropdownMenu(specialties: counterStore.specialties,  onSelected: _registerStore.setSpecialty),
+              } else if (_registerStore.role == "doctor" && (_registerStore.specialty == null || _registerStore.specialty!.isEmpty || _registerStore.localization == null || _registerStore.localization!.isEmpty)) {
+                ScaffoldMessenger.of(context).showSnackBar(
+                  const SnackBar(content: Text("Specialty and localization are mandatory for doctors")),
+                );
 
-            if (_registerStore.role == 'doctor')
-              TextField(
-                onChanged : _registerStore.setLocalization,
-                controller: _localizationController,
-                decoration: const InputDecoration(
-                  labelText: 'Localization',
-                ),
-              ),
-              ElevatedButton(
-                key: const Key('registerButton'),
-                onPressed: () async {
-                  // Store the BuildContext in a local variable
-                  final localContext = context;
-                  if (_registerStore.username.isEmpty || _registerStore.password.isEmpty || _registerStore.role == null || _registerStore.role!.isEmpty) {
-                    ScaffoldMessenger.of(context).showSnackBar(
-                      const SnackBar(content: Text("Username, password, and role are mandatory")),
-                    );
-                  } else if (_registerStore.role == "doctor" && (_registerStore.specialty == null || _registerStore.specialty!.isEmpty || _registerStore.localization == null || _registerStore.localization!.isEmpty)) {
-                    ScaffoldMessenger.of(context).showSnackBar(
-                      const SnackBar(content: Text("Specialty and localization are mandatory for doctors")),
+              } 
+              // role has to be set (not null)
+              else if (_registerStore.role == null) {
+                ScaffoldMessenger.of(context).showSnackBar(
+                  const SnackBar(content: Text("Role has to be set")),
+                );
+              }                  
+              else
+              {
+                UserRole roleEnum = UserRole.values.firstWhere((e) => e.toString() == 'UserRole.${_registerStore.role}');
+                int specialtyIndex = counterStore.specialties.indexOf(_registerStore.specialty ?? '');
+
+                try {
+                  Status value = await counterStore.performRegistration(
+                    username: _registerStore.username,
+                    password: _registerStore.password,
+                    role: roleEnum,
+                    specialty: specialtyIndex == -1 ? null : specialtyIndex,
+                    localization: _registerStore.localization,
+                  );
+
+
+                  if (value is Response) {
+                    // Show a toast message
+                    Fluttertoast.showToast(
+                      msg: "Registration successful",
+                      toastLength: Toast.LENGTH_SHORT,
+                      gravity: ToastGravity.BOTTOM,
+                      timeInSecForIosWeb: 2,
+                      backgroundColor: Colors.green,
+                      textColor: Colors.white,
+                      fontSize: 16.0
                     );
 
-                  } 
-                  // role has to be set (not null)
-                  else if (_registerStore.role == null) {
-                    ScaffoldMessenger.of(context).showSnackBar(
-                      const SnackBar(content: Text("Role has to be set")),
-                    );
-                  }                  
+                    // Wait for the toast to finish, then navigate
+                    Future.delayed(const Duration(seconds: 4), () {
+                      Navigator.pushReplacement(
+                        localContext,
+                        MaterialPageRoute(builder: (context) => const MyHomePage(title: 'Another Matiki Flutter Demo Home Page')),
+                      );
+                    });
+                  }
                   else
                   {
-                    UserRole roleEnum = UserRole.values.firstWhere((e) => e.toString() == 'UserRole.${_registerStore.role}');
-                    int specialtyIndex = counterStore.specialties.indexOf(_registerStore.specialty ?? '');
-
-                    try {
-                      Status value = await counterStore.performRegistration(
-                        username: _registerStore.username,
-                        password: _registerStore.password,
-                        role: roleEnum,
-                        specialty: specialtyIndex == -1 ? null : specialtyIndex,
-                        localization: _registerStore.localization,
-                      );
-
-
-                      if (value is Response) {
-                        // Show a toast message
-                        Fluttertoast.showToast(
-                          msg: "Registration successful",
-                          toastLength: Toast.LENGTH_SHORT,
-                          gravity: ToastGravity.BOTTOM,
-                          timeInSecForIosWeb: 2,
-                          backgroundColor: Colors.green,
-                          textColor: Colors.white,
-                          fontSize: 16.0
-                        );
-
-                        // Wait for the toast to finish, then navigate
-                        Future.delayed(const Duration(seconds: 4), () {
-                          Navigator.pushReplacement(
-                            localContext,
-                            MaterialPageRoute(builder: (context) => const MyHomePage(title: 'Another Matiki Flutter Demo Home Page')),
-                          );
-                        });
-                      }
-                      else
-                      {
-                        // print error
-                        ScaffoldMessenger.of(localContext).showSnackBar(
-                          SnackBar(content: Text(value.toString())),
-                        );
-                      }
-                    } catch (e) {
-                      // Handle any errors here
-
-                      // print error
-                      ScaffoldMessenger.of(localContext).showSnackBar(
-                        SnackBar(content: Text(e.toString())),
-                      );
-                    }
+                    // print error
+                    ScaffoldMessenger.of(localContext).showSnackBar(
+                      SnackBar(content: Text(value.toString())),
+                    );
                   }
-                },
-                child: const Text('Register'),
-              )
-              
-          ],
-        ),
-      ),
-      ),  
+                } catch (e) {
+                  // Handle any errors here
+
+                  // print error
+                  ScaffoldMessenger.of(localContext).showSnackBar(
+                    SnackBar(content: Text(e.toString())),
+                  );
+                }
+              }
+            },
+            child: const Text('Register'),
+          )
+          
+      ],
+      )
     );
   }
 }
