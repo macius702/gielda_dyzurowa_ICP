517bc9fbb2e627951b8aa8b83dd8c39ffa581ad4
Matiki <maciej.tomasz.krzeminski@gmail.com>
2024-06-16 18:45:22 +0200

    Fix a include

--- d/src/d_frontend/lib/show_usernames_screen.dart
+++ d/src/d_frontend/lib/show_usernames_screen.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_mobx/flutter_mobx.dart';
 import 'package:provider/provider.dart';
-import 'counter_store.dart';
+import 'package:d_frontend/counter_store.dart';
 
 class ShowUsernamesBody extends StatelessWidget {
   @override
