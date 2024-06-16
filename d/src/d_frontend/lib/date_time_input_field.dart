2499d02c29bfebbb46f40e29756f548675a84016
Matiki <maciej.tomasz.krzeminski@gmail.com>
2024-06-16 11:08:51 +0200

    Simpify  DateTimeInputField

--- d/src/d_frontend/lib/date_time_input_field.dart
+++ d/src/d_frontend/lib/date_time_input_field.dart
@@ -38,85 +38,48 @@ class _DateTimeInputFieldState extends State<DateTimeInputField> {
 
   @override
   Widget build(BuildContext context) {
-    return Column(
-      children: [
-        Row(
-          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-          children: [
-            Expanded(
-              child: TextFormField(
-                controller: dateController,
-                decoration: InputDecoration(labelText: '${widget.label} Date'),
-                readOnly: true,
-                onTap: () async {
-                  final date = await showDatePicker(
-                    context: context,
-                    initialDate: DateTime.parse(dateController.text),
-                    firstDate: DateTime.now(),
-                    lastDate: DateTime.now().add(Duration(days: 365)),
-                  );
-                  if (date != null) {
-                    dateController.text = DateFormat('yyyy-MM-dd').format(date);
-                    widget.onDateChanged(date);
-                  }
-                },
-              ),
-            ),
-            ElevatedButton(
-              onPressed: () async {
-                final date = await showDatePicker(
-                  context: context,
-                  initialDate: DateTime.parse(dateController.text),
-                  firstDate: DateTime.now(),
-                  lastDate: DateTime.now().add(Duration(days: 365)),
-                );
-                if (date != null) {
-                  dateController.text = DateFormat('yyyy-MM-dd').format(date);
-                  widget.onDateChanged(date);
-                }
-              },
-              child: Text('Select ${widget.label} Date'),
-            ),
-          ],
-        ),
-        Row(
-          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-          children: [
-            Expanded(
-              child: TextFormField(
-                controller: timeController,
-                decoration: InputDecoration(labelText: '${widget.label} Time'),
-                readOnly: true,
-                onTap: () async {
-                  final time = await showTimePicker(
-                    context: context,
-                    initialTime: parseTimeOfDay(timeController.text),
-                  );
-                  if (time != null) {
-                    timeController.text = formatTimeOfDay(time);
-                    widget.onTimeChanged(time);
-                  }
-                },
-              ),
-            ),
-            ElevatedButton(
-              onPressed: () async {
-                final time = await showTimePicker(
-                  context: context,
-                  initialTime: parseTimeOfDay(timeController.text),
-                );
-                if (time != null) {
-                  timeController.text = formatTimeOfDay(time);
-                  widget.onTimeChanged(time);
-                }
-              },
-              child: Text('Select ${widget.label} Time'),
-            ),
-          ],
-        ),
-      ],
-    );
-  }
+    return Row(
+  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+  children: [
+    Expanded(
+      child: TextFormField(
+        controller: dateController,
+        decoration: InputDecoration(labelText: '${widget.label} Date'),
+        readOnly: true,
+        onTap: () async {
+          final date = await showDatePicker(
+            context: context,
+            initialDate: DateTime.parse(dateController.text),
+            firstDate: DateTime.now(),
+            lastDate: DateTime.now().add(Duration(days: 365)),
+          );
+          if (date != null) {
+            dateController.text = DateFormat('yyyy-MM-dd').format(date);
+            widget.onDateChanged(date);
+          }
+        },
+      ),
+    ),
+    SizedBox(width: 10), // You can adjust this value as needed
+    Expanded(
+      child: TextFormField(
+        controller: timeController,
+        decoration: InputDecoration(labelText: '${widget.label} Time'),
+        readOnly: true,
+        onTap: () async {
+          final time = await showTimePicker(
+            context: context,
+            initialTime: parseTimeOfDay(timeController.text),
+          );
+          if (time != null) {
+            timeController.text = formatTimeOfDay(time);
+            widget.onTimeChanged(time);
+          }
+        },
+      ),
+    ),
+  ],
+);  }
 
   @override
   void didUpdateWidget(DateTimeInputField oldWidget) {
