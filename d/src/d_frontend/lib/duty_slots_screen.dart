3c3d962448f31212c0599164145c0c3b39529d5e
Matiki <maciej.tomasz.krzeminski@gmail.com>
2024-06-21 07:12:14 +0200

    Color action menu

--- d/src/d_frontend/lib/duty_slots_screen.dart
+++ d/src/d_frontend/lib/duty_slots_screen.dart
@@ -53,77 +53,99 @@ class DutySlotsBody extends StatelessWidget {
             ],
             rows: List<DataRow>.generate(
               counterStore.duty_slots.length,
-              (int index) => DataRow(
-                cells: <DataCell>[
-                  DataCell(
-                    Text(
-                      '${counterStore.duty_slots[index].hospitalId.username}',
-                      style: TextStyle(color: Colors.blue, fontSize: 16),
+              (index) {
+                Color color;
+                switch (counterStore.duty_slots[index].status) {
+                  case DutyStatus.open:
+                    color = Colors.white; // Normal color for open status
+                    break;
+                  case DutyStatus.pending:
+                    color = Colors.yellow; // Yellow color for pending status
+                    break;
+                  case DutyStatus.filled:
+                    color = Colors.green; // Green color for filled status
+                    break;
+                }
+
+                return DataRow(
+                  color: WidgetStateProperty.resolveWith((states) => color),
+                  cells: <DataCell>[
+                    DataCell(
+                      Text(
+                        '${counterStore.duty_slots[index].hospitalId.username}',
+                        style: TextStyle(color: Colors.blue, fontSize: 16),
+                      ),
                     ),
-                  ),
-                  DataCell(
-                    Text(
-                      '${counterStore.duty_slots[index].requiredSpecialty.name}',
-                      style: TextStyle(color: Colors.green, fontSize: 16),
+                    DataCell(
+                      Text(
+                        '${counterStore.duty_slots[index].requiredSpecialty.name}',
+                        style: TextStyle(color: Colors.green, fontSize: 16),
+                      ),
                     ),
-                  ),
-                  DataCell(
-                    Text(
-                      '${DateFormat('yyyy-MM-dd').format(DateTime.parse(counterStore.duty_slots[index].startDateTime))}',
-                      style: TextStyle(color: Colors.red, fontSize: 16),
+                    DataCell(
+                      Text(
+                        '${DateFormat('yyyy-MM-dd').format(DateTime.parse(counterStore.duty_slots[index].startDateTime))}',
+                        style: TextStyle(color: Colors.red, fontSize: 16),
+                      ),
                     ),
-                  ),
-                  DataCell(
-                    Text(
-                      '${counterStore.duty_slots[index].priceFrom}',
-                      style: TextStyle(color: Colors.purple, fontSize: 16),
+                    DataCell(
+                      Text(
+                        '${counterStore.duty_slots[index].priceFrom}',
+                        style: TextStyle(color: Colors.purple, fontSize: 16),
+                      ),
                     ),
-                  ),
-                  DataCell(
-                    Text(
-                      '${counterStore.duty_slots[index].priceTo}',
-                      style: TextStyle(color: Colors.orange, fontSize: 16),
+                    DataCell(
+                      Text(
+                        '${counterStore.duty_slots[index].priceTo}',
+                        style: TextStyle(color: Colors.orange, fontSize: 16),
+                      ),
                     ),
-                  ),
-                  DataCell(PopupMenuButton<String>(
-                    onSelected: (String value) {
-                      switch (value) {
-                        case 'Accept':
-                          print(
-                              'Accept action on value: ${counterStore.duty_slots[index]}');
-                          counterStore.accept_duty_slot(
-                              counterStore.duty_slots[index].id);
-                          break;
-                        case 'Delete':
-                          print(
-                              'Delete action on value: ${counterStore.duty_slots[index].id}');
-                          counterStore.delete_duty_slot(
-                              counterStore.duty_slots[index].id);
-                          break;
-                      }
-                    },
-                    itemBuilder: (BuildContext context) {
-                      if (counterStore.role == UserRole.hospital) {
-                        return <PopupMenuEntry<String>>[
-                          const PopupMenuItem<String>(
-                            key: Key('deleteMenuItem'),
-                            value: 'Delete',
-                            child: Text('Delete'),
-                          ),
-                        ];
-                      } else {
-                        return <PopupMenuEntry<String>>[
-                          const PopupMenuItem<String>(
-                            key: Key('acceptMenuItem'),
-                            value: 'Accept',
-                            child: Text('Accept'),
-                          ),
-                        ];
-                      }
-                    },
-                  )),
-                ],
-              ),
+                    DataCell(PopupMenuButton<String>(
+                      onSelected: (String value) {
+                        switch (value) {
+                          case 'Accept':
+                            print(
+                                'Accept action on value: ${counterStore.duty_slots[index]}');
+                            counterStore.accept_duty_slot(
+                                counterStore.duty_slots[index].id);
+                            break;
+                          case 'Delete':
+                            print(
+                                'Delete action on value: ${counterStore.duty_slots[index].id}');
+                            counterStore.delete_duty_slot(
+                                counterStore.duty_slots[index].id);
+                            break;
+                        }
+                      },
+                      itemBuilder: (BuildContext context) {
+                        if (counterStore.role == UserRole.hospital) {
+                          return <PopupMenuEntry<String>>[
+                            const PopupMenuItem<String>(
+                              key: Key('deleteMenuItem'),
+                              value: 'Delete',
+                              child: Text('Delete'),
+                            ),
+                          ];
+                        } else {
+                          assert(counterStore.role == UserRole.doctor);
+                          String value = counterStore.duty_slots[index].status == DutyStatus.open ? 'Accept' : 'Nothing';                            
+
+
+
+
+                          return <PopupMenuEntry<String>>[
+                            PopupMenuItem<String>(
+                              key: Key('acceptMenuItem'),
+                              value: value,
+                              child: Text(value),
+                            ),
+                          ];
+                        }
+                      },
+                    )),
+                  ],
+                );
+              },
             ),
           ),
         );
